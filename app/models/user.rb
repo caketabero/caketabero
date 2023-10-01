784d8bd46d6c5118219aabd9d540f619bd84d6b0
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-10-01 07:28:31 +0000

    Userモデルのバリデーション設定、devise設定変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { minimum: 1, maximum: 255 }
+  validates :email, presence: true, uniqueness: true, length: { minimum: 1, maximum: 255 }
+  validates :telephone_number, presence: true, length: { minimum: 9, maximum: 11 }
+  # validates :encrypted_password, presence: true, length: { minimum: 8, maximum:255 }
+  # validates :admin_flag, presence: true
+  # validates :deleted_flag, presence: true
 end
