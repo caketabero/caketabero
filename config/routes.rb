10b17a5f6e41dce148ce492fad00e65ea448a83a
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-10-02 11:20:03 +0000

    ユーザー一覧ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index,:edit,:destroy,:update]
+  resources :users, only: [:index,:edit,:destroy,:update] do
+    collection do
+      get 'sort'
+    end
+  end
+
   root :to => "homes#top"
   get 'home/about' => 'homes#about', as: 'about'
 resources :cakes
