5477aeb0e85ad840a8835332b6b561e104cee5c0
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-10-02 06:22:13 +0000

    ログインしていない場合はログインページにリダイレクトさせる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
-before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
-private
+  private
 
   def after_sign_out_path_for(resource)
     user_session_path
