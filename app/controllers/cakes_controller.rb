e6496a29d0727192895a35b688f5591550202c3a
ikehiro1 <136701716+ikehiro1@users.noreply.github.com>
2023-10-03 01:02:07 +0000

    cakes_controller追記

--- app/controllers/cakes_controller.rb
+++ app/controllers/cakes_controller.rb
@@ -2,4 +2,23 @@ class CakesController < ApplicationController
     def index
         
     end
-end
+    
+    def new
+      @cake = Cake.new
+    end
+    
+    def create
+        @cake = Cake.new(cake_params)
+        @cake.user_id = current_user.id
+      if @cake.save
+        redirect_to cakes_path
+      else
+        render :new  
+      end    
+    end
+    
+    private
+    def cake_params 
+      params.require(:cake).permit(:product_name, :price, :stock)     
+    end
+end 
