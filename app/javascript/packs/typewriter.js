79f8e156c34f2ee73e39f8f114d80b05a7c47ac8
ikehiro1 <136701716+ikehiro1@users.noreply.github.com>
2023-10-03 01:26:46 +0000

    typewriter追記

--- app/javascript/packs/typewriter.js
+++ app/javascript/packs/typewriter.js
@@ -1,14 +1,14 @@
 window.addEventListener('load', () => {
 //const text = 'ようこそ@user.さん ';
 let textDOM = document.getElementById('typewriter-text');
-console.log(textDOM);
+//console.log(textDOM);
 let userName = textDOM.getAttribute('data-current-user-name');
 //console.log(userName);
 const text = 'ようこそ' + userName + 'さん ';
 const delay = 75; 
 let index = 0;
 
-function type() {
+let type = () => {
   textDOM.innerHTML += text[index];
   index++;
   if (index >= text.length - 1) {
