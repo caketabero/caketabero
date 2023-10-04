64db705d8853bdda883ecdd401f2c97707ddb46a
ikehiro1 <136701716+ikehiro1@users.noreply.github.com>
2023-10-04 01:54:25 +0000

    論理削除後のemailについて

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,15 @@ class User < ApplicationRecord
   has_many :cakes
 
   validates :name, presence: true, length: { minimum: 1, maximum: 255 }
-  validates :email, presence: true, uniqueness: true, length: { minimum: 1, maximum: 255 }
+  validates :email, presence: true, uniqueness: true, length: { minimum: 1, maximum: 255 } #unless: "?"
   validates :telephone_number, presence: true, length: { minimum: 9, maximum: 11 }
   # validates :encrypted_password, presence: true, length: { minimum: 8, maximum:255 }
   # validates :admin_flag, presence: true
   # validates :deleted_flag, presence: true
+   
+ # def email_uniqueness
+ #  byebug
+ #  email_uniqueness
+ # end
   
-  def change
-    add_column :users, deleted_flag, :boolean, default: false
-  end
 end
