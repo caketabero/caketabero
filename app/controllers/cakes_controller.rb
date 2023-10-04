b245453b6fe3c9b2bc4030b88df9dbf99cfcfe86
ikehiro1 <136701716+ikehiro1@users.noreply.github.com>
2023-10-04 14:50:57 +0000

    ジャンル選択

--- app/controllers/cakes_controller.rb
+++ app/controllers/cakes_controller.rb
@@ -13,15 +13,48 @@ class CakesController < ApplicationController
 
     def new
       @cake = Cake.new
+      @genres = Genre.all
+      select_genres = @genres.pluck(:name)
+      select_genres.shift
+      @select_genres =  select_genres
     end
 
     def create
-     @cake = Cake.new(cake_params)
-      @cake.user_id = current_user.id
+    case params[:cake][:genre_id]
+    when "ケーキ" then 
+     params[:cake][:genre_id] = 2
+     @genre_id = params[:cake][:genre_id] 
+    when "タルト" then 
+     params[:cake][:genre_id] = 3
+     @genre_id = params[:cake][:genre_id] 
+    when "チョコレート系" then 
+     params[:cake][:genre_id] = 4
+     @genre_id = params[:cake][:genre_id] 
+    when "季節系" then 
+     params[:cake][:genre_id] = 5
+     @genre_id = params[:cake][:genre_id] 
+    when "その他" then 
+     params[:cake][:genre_id] = 6
+     @genre_id = params[:cake][:genre_id] 
+    when "未選択" then 
+     params[:cake][:genre_id] = 7
+     @genre_id = params[:cake][:genre_id]
+    end
+     @cake = Cake.new(
+       cake_name:params[:cake][:cake_name],
+       cost:params[:cake][:cost],
+       stock:params[:cake][:stock],
+       explanation:params[:cake][:explanation],
+       created_name:params[:cake][:created_name],
+       updated_name:params[:cake][:updated_name],
+       user_id:current_user.id,
+       genre_id:@genre_id,
+       )
+       @select_genres = @genre_id
      if @cake.save
       redirect_to cakes_path
      else
-      render :new
+      render 'new'
      end
     end
 
@@ -59,6 +92,6 @@ class CakesController < ApplicationController
 
     private
     def cake_params
-    params.require(:cake).permit(:cake_name, :cost, :stock)
+     params.require(:cake).permit(:cake_name, :cost, :stock, :genre_id, :explanation, :created_name, :updated_name)
     end
 end
