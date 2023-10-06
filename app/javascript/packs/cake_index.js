1147da4f3b3c9f6fb489c626efb0cb9583f5f0e4
Kazuki-Igarashi <vki.0504@gmail.com>
2023-10-06 07:39:45 +0000

    微調整

--- app/javascript/packs/cake_index.js
+++ app/javascript/packs/cake_index.js
@@ -3,45 +3,10 @@ if (location.href.slice( -5 ) == "cakes"){
     const cakeCheckboxs = document.querySelectorAll('.js__checkbox');
     cakeCheckboxs.forEach((checkbox) => {
       checkbox.addEventListener('change', (event) => {
-        const textDOM = document.getElementById('cart-text');
+        const textDOM = document.querySelector('cart-text');
         const js__checkbox = textDOM.getAttribute('input[type="checkbox"]:checked');
         const text = 'カートに' + js__checkbox + '個のアイテムが入っています ';
-        const delay = 0;
-        let index = 0;
-        const type= () => {
-          textDOM.innerHTML += text[index];
-          index++;
-          if (index >= text.length) {
-            clearInterval(intervalId);
-          }
-        }
-  
-        const intervalId = setInterval(type, delay);
-        
-        
-        cakeCheckboxs.forEach((checkbox) => {
-        checkbox.addEventListener('change', (event) => {
-        const Checkboxs = document.querySelectorAll('input[type="checkbox"]:checked');
-        const a = document.getElementsByClassName("js__checkbox");
-        const getCheckedCount = () => {
-            
-                let count = 0;
-            
-                for (let i = 0; i < a.length; i++) {
-                    if (a[i].checked) {
-                        count++;
-                    }
-                }
-                alert(count);
-                js__checkbox.addEventListener("click", getCheckedCount, false);
-            };
-          });
-        });
-      });
-    });
-      
-   
-    
+
     const cart = document.querySelectorAll(".cart__action");
     cart[0].addEventListener('click',() => {
     cart[0].classList.toggle("active");
